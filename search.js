e0bdf9ee9e45ac9676dc2f42a829c8de5d9d7438
anderconsu <anderconsu@gmail.com>
2023-06-20 21:25:23 +0200

    clase searchQuery creada y documentada. No puedo probar si funciona porque su server no va

--- search.js
+++ search.js
@@ -1,26 +1,35 @@
-import tvMovie from "./tvFilm.js";
-
 class searchQuery {
-    
-    async search(query){
-    let searched = [];
-    try{
-        let response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=true&language=es-ES&page=1`,{
-        method: 'GET',    
-        headers: {
-            accept:'application/json',
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTBjZjRlYjI1YTI1YTQ0NTQ5M2NiYzVhMjIyYTA4ZiIsInN1YiI6IjYzYWIzZmJlYzU2ZDJkMDA4OWFmYmI3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eSKbqW8m2aFGiuNnOmobjtIVdGJtFNl0DGQG7rRwhtw",
-            },
-          });
-        let data = await response.json();
-        searched = data.results;
-    }
-    catch (error){
-        console.error(error)
+  /*
+   * Busca de manera asíncrona películas y programas de TV utilizando la cadena de consulta proporcionada. 
+   * Devuelve un array de instancias de `tvMovie` que representan los resultados de la búsqueda.
+   *
+   * @param {string} query - La cadena de consulta a utilizar.
+   * @return {array} Un array de instancias de `tvMovie` que representan los resultados de la búsqueda.
+   */
+  async search(query) {
+    let searched = [];  // Declarar el array de resultados
+    try {
+      let response = await fetch(       // Ejecutar la petición
+        `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&language=es-ES&page=1`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTBjZjRlYjI1YTI1YTQ0NTQ5M2NiYzVhMjIyYTA4ZiIsInN1YiI6IjYzYWIzZmJlYzU2ZDJkMDA4OWFmYmI3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eSKbqW8m2aFGiuNnOmobjtIVdGJtFNl0DGQG7rRwhtw",
+          },
+        }
+      );
+      let data = await response.json();         // Obtener los datos y tranformarlos en un objeto
+      searched = data.results.forEach((element) => new tvMovie(element));   // Crear los objetos con los datos
+    } catch (error) {
+      console.error(error);
     }
     return searched;
-    }
-};
+  }
+}
 
-const search = new searchQuery();
-console.log(search.search("avengers"));
+export default searchQuery;     // Exportar el objeto
+
+// let search = new searchQuery();
+// search.search("robert");
