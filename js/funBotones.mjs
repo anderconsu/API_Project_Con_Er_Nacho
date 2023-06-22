6001a62bc795ba52312c3e0ab5bb712d24c680fd
anderconsu <anderconsu@gmail.com>
2023-06-22 17:01:44 +0200

    function added to buttons

---
+++ js/funBotones.mjs
@@ -0,0 +1,45 @@
+import renderGeneral from "./renderGeneral.mjs";
+
+class FunBotones {
+    constructor() {
+        this.renderGeneral = new renderGeneral();
+    }
+
+    addFunciones(plataforma) {
+        let accion = document.getElementById("accion");
+        accion.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.accion, plataforma);
+        })
+        let aventura = document.getElementById("aventura");
+        aventura.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.aventura, plataforma);
+        })
+        let animacion = document.getElementById("animacion");
+        animacion.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.animacion, plataforma);
+        })
+        let comedia = document.getElementById("comedia");
+        comedia.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.comedia, plataforma);
+        })
+        let crimen = document.getElementById("crimen");
+        crimen.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.crimen, plataforma);
+        })
+        let documental = document.getElementById("documental");
+        documental.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.documental, plataforma);
+        })
+        let drama = document.getElementById("drama");
+        drama.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.drama, plataforma);
+        })
+        let misterio = document.getElementById("misterio");
+        misterio.addEventListener("click", () => {
+            this.renderGeneral.filter(this.renderGeneral.categorias.categorias.misterio, plataforma);
+        })
+    }
+
+
+}
+export default FunBotones
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import renderGeneral from "./js/renderGeneral.mjs";
-
+import FunBotones from "./js/funBotones.mjs";
+let botones = new FunBotones();
+botones.addFunciones();
 let render = new renderGeneral();
 render.renderGeneral()
